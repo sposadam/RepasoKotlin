50e4fc30362cfbea437a1e4a6dc344b33d871efb
sposadam <sp1999m@gmail.com>
2026-04-08 18:32:15 -0500

    Condicionales

--- src/Main.kt
+++ src/Main.kt
@@ -15,7 +15,7 @@ fun main() {
     // valor previamente definido)
 
     // Para realizar un salida por consola se usa:
-    println("Variables de Tipo IMPLICITO")
+    /*println("Variables de Tipo IMPLICITO")
 
     /*Las Variables de Tipo IMPLICITO son aquellas a las cuales les definimos o asignamos un valor
      sin necesidad de especificar el tipo de variable, Ejemplo: Int, Float, String, Boleean, etc */
@@ -55,4 +55,72 @@ fun main() {
      Para la Multiplicación se usa: *
      Y para la división usamos el simbolo: /
      */
+
+    /* Si deseamos imprimir algo por consola sin necesidad de usar varios saltos de línea,
+    * lo podemos realizar usando el println (""" con tres comillas dobles """).
+    * Al usarlo de esta manera Kotlin ya identifica que queremos imprimir varias lineas
+    *  pero dentro de un solo println() */
+
+    println("""
+        Opcion 1 
+        Opcion 2
+        Opcion 3
+    """.trimIndent()) */
+
+    //CONVERSIONES TIPOS DE DATOS
+
+    /*
+    var total = "20" // Esta variable es tipo String porque esta entre ""
+
+    var convertir = total.toInt() // Por medio de esto convertimos un String a un Integer o Entero
+
+    // INGRESAR DATOS POR TECLADO
+
+    println("Ingrese el primer valor numerico")
+    var n1 = readln().toInt() /*  Con este comando el programa entiende
+     que se van a ingresar datos de manera manual*/
+
+    println("Ingrese el segundo valor numerico")
+    var n2 = readln().toInt() /*  Con este comando el programa entiende
+     que se van a ingresar datos de manera manual*/
+
+    var suma = n1 + n2
+
+    println("La suma entre ${n1} y ${n2} es $suma")
+
+    // ESTRUCTURAS
+
+    // Condicionales IF - ELSE
+
+    /*
+    if (Evalúa una condición) {
+        Instrucción - Verdadera
+    } else{
+        Instrucción - Falsa
+    }
+     */
+
+    // EJEMPLO
+    println("INGRESE SU EDAD")
+    var edad = readln().toInt()
+
+    if (edad >= 18) {
+        println("Eres mayor de edad")
+    }
+    else {
+        println("Eres menor de edad")
+    }
+    */
+    println("INGRESE UN NUMERO ENTERO")
+    var numero = readln().toInt()
+
+    if (numero > 0) {
+        println("EL NUMERO ES POSITIVO")
+    } else if (numero < 0) {
+        println("EL NUMERO ES NEGATIVO")
+    } else {
+        println("EL NUMERO DIGITADO ES CERO")
+    }
+
+
 }
